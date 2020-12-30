43470e5e329aeb3701b349e9eeb3141789d81445
Krystepan <krystian.konias@gmail.com>
2020-12-30 09:13:54 +0100

    firts commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Dupsko</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consectetur tenetur
+          repellat similique fugit, exercitationem error officia saepe quos excepturi officiis quam
+          nisi recusandae quaerat dicta autem vel obcaecati? Assumenda?
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
